c05e193854566c315bc95981a47bf8c25af759d7
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2023-10-11 23:36:58 -0700

    new file:   Talleres/Taller6/Taller6.c

---
+++ Talleres/Taller6/Taller6.c
@@ -0,0 +1,17 @@
+/*
+    Taller 6 Ejercicios Apuntadores y Argumentos de funciones
+    Gonzalez Ceseña Adan 372799
+    Grupo 932
+    Fecha de Creacion 11/10/2023
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//  Main
+
+int main()
+{
+    
+
+}
