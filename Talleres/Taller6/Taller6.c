78e4f8f480bac05eac69afd84321e5a63138a957
Andan <141715477+AdanCC21@users.noreply.github.com>
2023-10-12 13:24:59 -0700

    Actualizar Taller6.c

--- Talleres/Taller6/Taller6.c
+++ Talleres/Taller6/Taller6.c
@@ -8,14 +8,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Prototipos
+int multiplicarPorDos (int p);
+
+
 //  Main
 
 int main()
 {
     int *ptr;
-    int x;
+    int x, r;
     x=2;
     ptr = &x;
     printf("X es igual a %d", ptr);
+    system("Pause");
+    r=multiplicarPorDos (ptr);
+    printf("Su valor multiplicado es %d", r);
 
 }
+
+//Funciones
+int multiplicarPorDos (int p)
+{
+    int b;
+    b=p*2;
+    return b;
+} 
