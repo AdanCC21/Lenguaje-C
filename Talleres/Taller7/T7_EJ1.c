94093df669c576675aa3ddda9c20463b0ade5612
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2023-10-23 16:46:42 -0700

    new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   Talleres/Taller7/T7_EJ1.c 	new file:   Talleres/Taller7/T7_EJ2.c P 	new file:   Talleres/Taller7/T7_Ej3.c 	new file:   Talleres/Taller7/output/T7_EJ1.exe 	new file:   Talleres/Taller7/output/T7_EJ2.exe

---
+++ Talleres/Taller7/T7_EJ1.c
@@ -0,0 +1,22 @@
+/*  
+    Taller 7 Ejercicio 1
+    Gonzalez Cesena Adan 372799
+    Grupo 932
+    23/10/2023
+*/
+
+#include <stdio.h>
+
+
+int main()
+{
+    char *ptr[3];
+    int i;
+    ptr[0]="ADAN";
+    ptr[1]="DRAKE";
+    ptr[2]="EDDY";
+    for(i=0;i<3;i++)
+    {
+        printf("Nombre : %s\n",ptr[i]);
+    }
+}
