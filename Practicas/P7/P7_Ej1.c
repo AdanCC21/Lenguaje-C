7d33991fe82a9db59e30f16802d8a4d267f9d96e
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2023-11-07 08:52:24 -0800

    modified:   Practicas/P7/P7_Ej1.c 	new file:   Practicas/P7/P7_Ej2.c 	new file:   Practicas/P7/output/P7_Ej1.exe 	modified:   Talleres/T_8/T8_Ej1.c 	modified:   Talleres/T_8/output/T8_Ej1.exe

--- Practicas/P7/P7_Ej1.c
+++ Practicas/P7/P7_Ej1.c
@@ -1,9 +1,45 @@
+/*
+    Gonzalez Cesena Adan 372799
+    Practica 7
+    G-932 06/11/2023
+*/
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-
-int main()
+int main(int argc, char *argv[])
 {
-    
+    int res,num1,num2;
+    num1=atoi(argv[1]);
+    num2=atoi(argv[2]);
+
+    printf("Numero total de argumentos: %d\n", argc);
+
+    for (int i = 0; i < argc; i++)
+    {
+        printf("Argumento %d: %s\n", i, argv[i]);
+    }
+    printf("______________________________\n");
+
+    res = num1+num2;
+    printf("Su suma es : %d\n",res);
+
+    res= num2-num1;
+    printf("Su resta es : %d\n",res);
+
+    res= num1*num2;
+    printf("Su multiplicacion es : %d\n",res);
+
+    float div;
+
+    if(num1==0 || num2==0)
+    {
+        printf("No es posible dividir entre 0\n");
+    }
+    else
+    {
+        div= (float)num1 / (float)num2;
+        printf("Su Division es : %f\n",div);
+    }
+
+    return 0;
 }
---
+++ Practicas/P7/P7_Ej2.c
@@ -0,0 +1,13 @@
+/*
+    Gonzalez Cesena Adan 372799
+    Practica 7 EJ2
+    G-932 06/11/2023
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int n;
+}
