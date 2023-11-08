9a5102abe00127b30790a4bdea1f18f9394a9054
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2023-11-07 16:53:30 -0800

    modified:   Practicas/P7/P7_Ej2.c 	new file:   Practicas/P7/libad.h 	new file:   Practicas/P7/output/P7_Ej2.exe

--- Practicas/P7/P7_Ej2.c
+++ Practicas/P7/P7_Ej2.c
@@ -6,8 +6,36 @@
 
 #include <stdio.h>
 #include <string.h>
+#include "libad.h"
+
+void My_Min (char cad[], int lon);
 
 int main()
 {
-    int n;
+    int n,i;
+    char cad[30];
+    n=strlen(cad);
+
+    printf("Ingrese su cadena\n");
+    gets(cad);
+    My_Min(cad,n);
+    for(i=0;i<n;i++)
+    {
+        //strcmp;
+    }
+}
+
+void My_Min (char cad[], int lon)
+{
+    int i;
+    for(i=0;i<lon;i++)
+    {
+        if(cad[i] >='A')
+        {
+            if(cad[i] <= 'Z')
+            {
+                cad[i]+=32;
+            }
+        }
+    }
 }
